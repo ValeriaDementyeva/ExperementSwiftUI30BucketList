f4364856fe83a5f6dddc4fb24e8236efa2ab9e5b
Valerai Dementyeva <lera.dementjeva@yandex.ru>
2023-09-29 13:46:16 +0300

    Initial Commit

---
+++ ExperementSwiftUI30BucketList/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  ExperementSwiftUI30BucketList
+//
+//  Created by Валерия Дементьева on 29.09.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ExperementSwiftUI30BucketList/ExperementSwiftUI30BucketListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExperementSwiftUI30BucketListApp.swift
+//  ExperementSwiftUI30BucketList
+//
+//  Created by Валерия Дементьева on 29.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct ExperementSwiftUI30BucketListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
